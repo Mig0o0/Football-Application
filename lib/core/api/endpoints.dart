e7dd6d10a8150f088428e8b8cef2b415d69cebad
Mohamed Gamal <62902365+Mig0o0@users.noreply.github.com>
2023-01-17 12:58:21 +0200

    Update endpoints.dart

--- lib/core/api/endpoints.dart
+++ lib/core/api/endpoints.dart
@@ -1,6 +1,6 @@
 class Endpoints{
   static const String baseUrl = "https://api.sportradar.us/soccer/trial/v4/en";
-  static const String _apiKey = "fakk4pqrpxf7rzshfkr34vps";
+  static const String _apiKey = "<YOUR_API_KEY>";
 
 
   static const String allCompetitions = "$baseUrl/competitions.json?api_key=$_apiKey";
